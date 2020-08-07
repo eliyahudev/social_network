e151c150d133fdc27870259cae95c91cce666f22
eliyahudev <eliyahulevyy@gmail.com>
2020-08-07 18:44:09 +0300

    add counter for the edges and the nodes

--- graph_creator.py
+++ graph_creator.py
@@ -2,16 +2,17 @@ import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
+import numpy as np
 
 
 def unionSpeakers(speaker):
     new_speaker = dict()
     j = 0
     for i in speaker:
-        if str(i).__eq__('nan') :
+        if str(i).__eq__('nan'):
             continue
         new_speaker[j] = i
-        if re.search('YOUNG BRUCE|WAYNE|BRUCE',i):
+        if re.search('YOUNG BRUCE|WAYNE|BRUCE', i):
             new_speaker[j] = 'BATMAN'
         if re.search('DUCARD/RA\'S AL GHUL|RA\'S AL GHUL', i):
             new_speaker[j] = 'DUCARD'
@@ -28,8 +29,8 @@ def batmaCreateGraph(g, script):
     script_df = pd.read_excel(script)
     speaker = unionSpeakers(script_df['speaker'])
     # addin edges to the graph
-    for i in range(len(speaker)-1):
-        g.add_edge(speaker[i], speaker[i+1])
+    for i in range(len(speaker) - 1):
+        g.add_edge(speaker[i], speaker[i + 1])
     return g
 
 
@@ -40,9 +41,17 @@ def thorCreatGraph(g, script):
     # get data from xlsx file
     script_df = pd.read_excel(script)
     speaker = script_df['speaker']
-    for i in range(len(speaker)-1):
-        g.add_edge(speaker[i], speaker[i+1])
+    for i in range(len(speaker) - 1):
+        g.add_edge(speaker[i], speaker[i + 1])
     return g
+
+
+def count_edges_and_nodes(g):
+    adj_met = nx.to_numpy_matrix(g)
+    nodes_num = len(adj_met)
+    edges_num = (np.ones((len(adj_met))).transpose() @ adj_met) @ np.ones(len(adj_met))
+    return nodes_num, int(edges_num)
+
 
 def printGraph(g):
     """vizualization of a graph using mathplotlib library
@@ -52,8 +61,11 @@ def printGraph(g):
     plt.show()
 
 
-# df = pd.read_excel('xl_files/script_batman.xlsx')
+# main
 g = thorCreatGraph(nx.MultiGraph(), 'xl_files/script_thor.xlsx')
-printGraph(g)
+printGraph(g)  # print AB graph
+print(count_edges_and_nodes(g))  # print #nodes(left) & #edges(right)
+
 g = batmaCreateGraph(nx.MultiGraph(), 'xl_files/script_batman.xlsx')
-printGraph(g)
+printGraph(g)  # print AB graph
+print(count_edges_and_nodes(g))  # print #nodes(left) & #edges(right)
